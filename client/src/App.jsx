4a5d6918acae3eb931526393ded91e9440631f0e
Alen Chacko <alenchacko.off@gmail.com>
2023-12-31 13:48:49 +0530

    Header and Footer completed

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+import Footer from "./components/Footer";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className='container py-3 mx-auto flex-grow'>
+        lihfvodi
+      </main>
+      <Footer />
+    </div>
+  </BrowserRouter>
+  );
+};
+
+export default App;
