be26eac8d032c026c8d2b9be287192cf4ed6a74a
Alen Chacko <alenchacko.off@gmail.com>
2024-01-04 01:19:38 +0530

    user register,update logout completed, but some issue with password updating

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,5 @@
 import User from "../models/userModel.js";
 import handler from "express-async-handler";
-import jwt from "jsonwebtoken";
 import generateToken from "../utils/generateToken.js";
 
 const loginUser = handler(async (req, res) => {
@@ -21,4 +20,63 @@ const loginUser = handler(async (req, res) => {
   }
 });
 
-export { loginUser };
+const registerUser = handler(async (req, res) => {
+    const { name, email, password } = req.body
+  
+    const userExists = await User.findOne({ email })
+  
+    if (userExists) {
+      res.status(400)
+      throw new Error("User already Exists")
+    }
+  
+    const user = await User.create({ name, email, password })
+  
+    if (user) {
+      generateToken(res, user._id)
+      res.status(201)
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      })
+    } else {
+      res.status(400)
+      throw new Error("Invalid User Credentials")
+    }
+  })
+  const updateUserProfile = handler(async (req, res) => {
+    const user = await User.findById(req.body._id)
+  
+    if (user) {
+      user.name = req.body.name || user.name
+      user.email = req.body.email || user.email
+      if (req.body.password) user.password = req.body.password
+      await user.save()
+      res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      })
+    } else {
+      res.status(404)
+      throw new Error("User Not Found")
+    }
+  })
+
+  const logoutUser = handler(async (req, res) => {
+    res.cookie("jwt", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    })
+    res.cookie("connect.sid", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    })
+    res.status(200).json({
+      message: "Logged Out Successfully",
+    })
+  })
+
+export { loginUser,registerUser,updateUserProfile,logoutUser };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,6 +1,9 @@
-import { loginUser } from "../controllers/userController.js";
+import { loginUser, logoutUser, registerUser, updateUserProfile } from "../controllers/userController.js";
 import express from 'express'
 const router = express.Router()
 
 router.route("/login").post(loginUser)
+router.route("/register").post(registerUser)
+router.route("/update").put(updateUserProfile)
+router.route("/logout").get(logoutUser)
 export default router
