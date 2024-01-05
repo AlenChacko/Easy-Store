b7130db7c2a70cff803926a467a314387a5a525e
Alen Chacko <alenchacko.off@gmail.com>
2024-01-05 11:01:32 +0530

    login completed frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -22,6 +22,7 @@ const router = createBrowserRouter(
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path="/cart" element={<CartScreen />} />
+      <Route path="/login" element={<LoginScreen />} />
     </Route>
   )
 );
--- client/src/store.js
+++ client/src/store.js
@@ -1,15 +1,17 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "./slices/apiSlice";
-import cartReducer from './slices/cartSlice'
+import { configureStore } from "@reduxjs/toolkit"
+import { apiSlice } from "./slices/apiSlice"
+import cartReducer from "./slices/cartSlice"
+import userReducer from "./slices/userSlice"
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
-    cart:cartReducer
+    cart: cartReducer,
+    user: userReducer,
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
-});
+})
 
-export default store;
+export default store
